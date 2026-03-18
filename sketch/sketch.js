d07790939f606fa68d7d4815194f209ac1fc6866
harold fu <waterdude551@gmail.com>
2026-03-18 11:05:35 -0400

    moved files into sketch dir

---
+++ sketch/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(800, 600);
+    // prints show up in Inspect Element -> Console
+    print("hello world!");
+}
+
+
+function draw() {
+    textSize(24);
+    background(220);
+    
+    text("Hello World!", 0,0);
+}
+
