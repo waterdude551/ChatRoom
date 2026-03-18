d9feb0f75da2a0e46b4aef5b7351aa68c94f8594
harold fu <waterdude551@gmail.com>
2026-03-18 11:06:06 -0400

    fonts in, glitch start

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,14 +1,34 @@
+let font;
+let boldFont;
+let italicFont;
+let boldItalicFont;
+
+let glitchShader;
+
+function preload() {
+    glitchShader = loadShader('shader.vert', 'shader.frag');
+}
+
+function loadFonts() {
+    font = loadFont("/assets/fonts/UbuntuMono-Regular.ttf");
+    boldFont = loadFont("/assets/fonts/UbuntuMono-Bold.ttf");
+    italicFont = loadFont("/assets/fonts/UbuntuMono-Italic.ttf");
+    boldItalicFont = loadFont("/assets/fonts/UbuntuMono-BoldItalic.ttf");
+}
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(innerWidth, innerHeight, WEBGL);
+    loadFonts();
     // prints show up in Inspect Element -> Console
     print("hello world!");
 }
 
 
 function draw() {
+    textFont(font);
     textSize(24);
-    background(220);
+    background(20);
     
-    text("Hello World!", 0,0);
+    text("Hello World!", -400, -200);
 }
 
