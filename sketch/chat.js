81584e72dce62aa8b7795e713102a9171e7ba72a
harold fu <waterdude551@gmail.com>
2026-04-08 10:27:30 -0400

    fixed errors and load fonts

--- sketch/chat.js
+++ sketch/chat.js
@@ -1,11 +1,8 @@
-let WHITE = color(204, 204, 204);
-let BLACK = color(10,10,10);
 let FRAMES_BETWEEN_CHARS = 5;
 let MARGIN_SIZE = 40;
 
 class Choice {
     constructor(action, message) {
-        this.index = index;
         this.action = action;
         this.message = message;
         this.messageShowing = "";
@@ -18,23 +15,29 @@ class Choice {
         this.x = MARGIN_SIZE;
         this.y;
         this.width = width - 2 * MARGIN_SIZE;
-        this.height = ;
+        this.height;
     }
-
-    
 }
 
-function displayChoices() {
+function displayChoices(...choices) { // variable length Choice array
     print("called displayChoices");
-    
+    let y = height - MARGIN_SIZE;
+    for (var i = 0; i < choices.length; i++) {
+
+    }
 }
 
 
 
 function drawChatScreen() {
     background(BLACK);
+
+    fill(WHITE);
+    textFont(font);
+    textSize(36);
+    text("hello chat world!",MARGIN_SIZE,height-MARGIN_SIZE-12);
 }
 
 function drawChat() {
-
+    drawChatScreen();
 }
--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,8 +1,27 @@
 let currentMode; // 0 for chat, 1 for room
+let WHITE;
+let BLACK;
+
+function preload() {
+
+}
+
+function loadFonts() {
+    font = loadFont("/sketch/assets/fonts/UbuntuMono-Regular.ttf");
+    boldFont = loadFont("/sketch/assets/fonts/UbuntuMono-Bold.ttf");
+    italicFont = loadFont("/sketch/assets/fonts/UbuntuMono-Italic.ttf");
+    boldItalicFont = loadFont("/sketch/assets/fonts/UbuntuMono-BoldItalic.ttf");
+    // print("loaded fonts!");
+}
 
 function setup() {
+    WHITE = color(204);
+    BLACK = color(10);
+    loadFonts();
     createCanvas(1024, 768);
+    
     frameRate(60);
+    currentMode = 0;
     // prints show up in Inspect Element -> Console
     print("hello world!");
 }
