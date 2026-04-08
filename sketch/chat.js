a1aadcb7a3cc718bdfc4190ff7e3db8ef5e5388a
harold fu <waterdude551@gmail.com>
2026-04-08 10:57:35 -0400

    action display

--- sketch/chat.js
+++ sketch/chat.js
@@ -1,5 +1,7 @@
 let FRAMES_BETWEEN_CHARS = 5;
 let MARGIN_SIZE = 40;
+let FONT_SIZE = 28;
+let showingChoices = false;
 
 class Choice {
     constructor(action, message) {
@@ -21,12 +23,18 @@ class Choice {
 
 function displayChoices(...choices) { // variable length Choice array
     print("called displayChoices");
-    let y = height - MARGIN_SIZE;
-    for (var i = 0; i < choices.length; i++) {
-
+    let y = height - MARGIN_SIZE - choices.length*FONT_SIZE/2;
+    for (var i = choices.length - 1; i >= 0; i--) {
+        choices[i].y = y
+        choices[i].height = FONT_SIZE*2.5;
+        y -= choices[i].height;
+        text(choices[i].action, choices[i].x, choices[i].y);
     }
 }
 
+function typeText() {
+
+}
 
 
 function drawChatScreen() {
@@ -34,8 +42,12 @@ function drawChatScreen() {
 
     fill(WHITE);
     textFont(font);
-    textSize(36);
-    text("hello chat world!",MARGIN_SIZE,height-MARGIN_SIZE-12);
+    textSize(FONT_SIZE);
+    if (!showingChoices) {
+        
+        showingChoices = true;
+    }
+    displayChoices(new Choice("(Action 1)", "Message message message 1."), new Choice("(Action 2)", "Message message message 2."));
 }
 
 function drawChat() {
