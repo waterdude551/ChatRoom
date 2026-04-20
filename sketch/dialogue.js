5c659503f634ca9b9b542fc2fdc6e855b28a6c3b
waterdude551 <waterdude551@gmail.com>
2026-04-20 01:58:35 -0400

    chat[1]

--- sketch/dialogue.js
+++ sketch/dialogue.js
@@ -23,5 +23,42 @@ function loadDialogue() {
         new Message("A", "try to stay calm. you just woke up, so get your bearings", 15),
 
         new Message("A", "take a look around your room. we can talk after :)")
-    ]
+    ];
+
+    chats[1] = [
+        new Prompt("A", "how's the room?",1,3),
+        new Choice("(Satisfactory)","It's good. Comfortable, even.",2),
+        new Message("A","i'm glad you find it to your liking!",5),
+        new Choice("(Lacking)","It's a little bare. There's not much in here.",4),
+        new Message("A","...sorry about that :( maybe you'll learn to love it lol", 5),
+
+        new Message("A","look, i'm gonna let you talk to a few other people tomorrow.",6),
+        new Prompt("A","i'll check back in some point later",7,9), // 6
+        new Choice("(Accept)","Alright. Looking forward to it. ",8),
+        new Message("A","glad you're cooperating! hope you enjoy talking to these people",11),
+        new Choice("(Question)","Wait, why? What should I do? Where are you going?",10),
+        new Message("A","i'll just be working. your work is basically talking to these people!",11),
+
+        new Message("A","you should get some proper sleep now. we'll talk again soon.")
+    ];
+
+    chats[2] = [
+        new Prompt("","",,),
+        new Choice("()","",),
+        new Message("","",),
+        new Choice("()","",),
+        new Message("","",),
+
+        new Prompt("","",,),
+        new Choice("()","",),
+        new Message("","",),
+        new Choice("()","",),
+        new Message("","",),
+
+        new Prompt("","",,),
+        new Choice("()","",),
+        new Message("","",),
+        new Choice("()","",),
+        new Message("","",),
+    ];
 }
